2e27bd50f280f2cf85005ad4cc5593916577aa8a
therafk <therafk@hotmail.com>
2018-06-19 21:33:11 +0100

    Admin Pages wievs

---
+++ CS180619754/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,31 @@
+﻿using CS180619754.Models.Data;
+using CS180619754.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CS180619754.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Declare list of PageVM
+            List<PageVM> pagesList;
+            
+            //using statement closes down connection
+            using (Db db =new Db())
+            {
+                //Initialize the list
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Return view with list
+
+            return View(pagesList);
+        }
+    }
+}
---
+++ CS180619754/Models/Data/Db.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CS180619754.Models.Data
+{
+    public class Db : DbContext
+    {
+        public DbSet<PageDTO> Pages { get; set; }
+    }
+}
